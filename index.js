6744bf3f0da52c6bb502b9399045c1bcf5cd762b
Jamie Bryson <jamieab98@gmail.com>
2025-07-15 17:23:57 -0400

    Finished the assignment

--- index.js
+++ index.js
@@ -29,25 +29,64 @@ let day27TempF = 74
 let day28TempC = 17
 let day29TempF = 76
 let day30TempC = 29
+
 //* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
 let day2TempF = (day2TempC * (9/5) + 32)
-console.log(day2TempF)
+let day4TempF = (day4TempC * (9/5) + 32)
+let day6TempF = (day6TempC * (9/5) + 32)
+let day8TempF = (day8TempC * (9/5) + 32)
+let day10TempF = (day10TempC * (9/5) + 32)
+let day12TempF = (day12TempC * (9/5) + 32)
+let day14TempF = (day14TempC * (9/5) + 32)
+let day16TempF = (day16TempC * (9/5) + 32)
+let day18TempF = (day18TempC * (9/5) + 32)
+let day20TempF = (day20TempC * (9/5) + 32)
+let day22TempF = (day22TempC * (9/5) + 32)
+let day24TempF = (day24TempC * (9/5) + 32)
+let day26TempF = (day26TempC * (9/5) + 32)
+let day28TempF = (day28TempC * (9/5) + 32)
+let day30TempF = (day30TempC * (9/5) + 32)
+
+let day1TempC = ((day1TempF - 32 ) * (5/9))
+let day3TempC = ((day3TempF - 32 ) * (5/9))
+let day5TempC = ((day5TempF - 32 ) * (5/9))
+let day7TempC = ((day7TempF - 32 ) * (5/9))
+let day9TempC = ((day9TempF - 32 ) * (5/9))
+let day11TempC = ((day11TempF - 32 ) * (5/9))
+let day13TempC = ((day13TempF - 32 ) * (5/9))
+let day15TempC = ((day15TempF - 32 ) * (5/9))
+let day17TempC = ((day17TempF - 32 ) * (5/9))
+let day19TempC = ((day19TempF - 32 ) * (5/9))
+let day21TempC = ((day21TempF - 32 ) * (5/9))
+let day23TempC = ((day23TempF - 32 ) * (5/9))
+let day25TempC = ((day25TempF - 32 ) * (5/9))
+let day27TempC = ((day27TempF - 32 ) * (5/9))
+let day29TempC = ((day29TempF - 32 ) * (5/9))
 
 //! Start the calculation of the total temperatures
+let tot_temperature_in_celsius = day1TempC + day2TempC + day3TempC + day5TempC + day6TempC + day7TempC + day8TempC + day9TempC + day10TempC + day11TempC + day12TempC + day13TempC + day14TempC + day15TempC + day16TempC + day17TempC + day18TempC + day19TempC + day20TempC + day21TempC + day22TempC + day23TempC + day24TempC + day25TempC + day26TempC + day27TempC + day28TempC + day29TempC + day30TempC
+let tot_temperature_in_fahrenheit = day1TempF + day2TempF + day3TempF + day4TempF + day5TempF + day6TempF + day7TempF + day8TempF + day9TempF + day10TempF + day12TempF + day13TempF + day14TempF + day15TempF + day16TempF + day17TempF + day18TempF + day19TempF + day20TempF + day21TempF + day22TempF + day23TempF + day24TempF + day25TempF + day26TempF + day27TempF + day28TempF + day29TempF + day30TempF
+
 //* Then apply the conversion to calculate the total in the other unit of measurement
 //* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
+console.log(tot_temperature_in_celsius)
+console.log(tot_temperature_in_fahrenheit)
 
 //! Start the calculation of the average temperatures
 //* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
+let avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 30
+let avg_temperature_in_celsius = tot_temperature_in_celsius / 30
 
 //! Console.log the results for your own inspection if you'd like
+console.log(avg_temperature_in_fahrenheit)
+console.log(avg_temperature_in_celsius)
 
 //! After creating the four variables mentioned above, uncomment the following lines
 //* This way you can export them to the test file, this is essential for the tests to work
 
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
+    tot_temperature_in_fahrenheit,
+    tot_temperature_in_celsius,
+    avg_temperature_in_fahrenheit,
+    avg_temperature_in_celsius
 };
